ec87949ee734273f7a29074706f6874277f4cf66
Rafaella Cintra <rafaellascintra@gmail.com>
2022-05-04 18:10:13 -0300

    feat: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 import { HttpPostParams } from '@/data/protocols/http'
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
   }
 }
