582d116f411e55ffefe6f8db18bdf3da29b71dfe
Rafaella Cintra <rafaellascintra@gmail.com>
2022-05-04 17:33:24 -0300

    feat: ensure AxiosHttpClient calls axios with correct url with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -11,10 +11,10 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct url', async () => {
+  test('Should call axios with correct url and verb', async () => {
     const url = faker.internet.url()
     const sut = makeSut()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
