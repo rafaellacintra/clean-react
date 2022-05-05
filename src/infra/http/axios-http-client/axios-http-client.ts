312acb37af6f3dd62e0df4ca12adfdf9605d4acf
Rafaella Cintra <rafaellascintra@gmail.com>
2022-05-05 08:59:52 -0300

    feat: implements Axios HttpPostClient

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
-import { HttpPostParams } from '@/data/protocols/http'
-export class AxiosHttpClient {
-  async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+import { HttpPostClient, HttpPostParams, HttpReponse } from '@/data/protocols/http'
+
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpReponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
